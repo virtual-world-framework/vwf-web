512c5c0f83558c68a52cc03cff8ce688a15a97ad
Jess Martin <jessmartin@gmail.com>
2014-01-20 18:17:24 -0500

    Grab the VWF id from the query line.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,38 @@ function setFocusPongFrame() {
     iframe.contentWindow.focus();
 }
 
+function preparePongFrame() {
+    var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+    })(window.location.search.substr(1).split('&'));
+
+    var id = qs["id"];
+    if (id == undefined) {
+        id = makeid();
+    }
+
+    // Load the iframe with the correct URL
+    var vwfServer = "http://development.virtual.wf"; // "http://apps.virtual.wf";
+    var pongUrl = vwfServer + "/vwf-pong/" + id;
+    var iframe = $("#pongFrame")[0];
+    iframe.src = pongUrl;
+
+    // Fill the form to copy with the share URL
+    var vwfWebServer = "http://virtual.wf";
+    var pongShareUrl = vwfWebServer + "?id=" + id;
+    $('#vwfPongId').val(pongShareUrl);
+
+    setTimeout(setFocusPongFrame, 100);
+}
+
 function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -28,25 +60,12 @@ function setUpCopy() {
             copyButton.removeClass("btn-info");
             copyButton.text("Copied!");
             copyButton.addClass("btn-success");
-        } );
+        });
     } );
 }
 
 $(document).ready(function() {
-    var id = makeid();
-    var vwfServer = "http://development.virtualworldframework.com"; // "http://apps.virtual.wf";
-    var pongUrl = vwfServer + "/vwf-pong/" + id;
-
-    $('#vwfPongId').val(pongUrl);
-    var iframe = $("#pongFrame")[0];
-    iframe.src = pongUrl;
-
-    setTimeout(setFocusPongFrame, 100);
-
-    $('.jumbotron').tooltip({
-        selector: "[data-toggle=tooltip]",
-        container: "body"
-    });
+    preparePongFrame();
 
     $( "body" ).keydown(function( event ) {
         switch( event.which ) {
